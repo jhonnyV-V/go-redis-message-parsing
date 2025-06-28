2614f5da2864db6465f586e74f80b50d6c359be8
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-06-27 20:16:14 -0400

    feat: boolean, integer and nil type

--- parser/parser.go
+++ parser/parser.go
@@ -68,6 +68,67 @@ func deserialiseBulkString(raw string) (interface{}, error)  {
 	return toParse[:valuesToParse], nil
 }
 
+func deserialiseInteger(raw string) (int, error) {
+	value := 0
+	rawValues := ""
+
+	for i := 1; i < len(raw); i++ {
+		if isCRLF(raw, i) {
+			break
+		}
+		rawValues += string(raw[i])
+	}
+
+	value, err := strconv.Atoi(rawValues)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to parse int (%q) err: %w", rawValues, err)
+	}
+
+	return value, nil
+}
+
+func deserialiseBool(raw string) (bool, error) {
+	if raw[1] == 't' {
+		return true, nil
+	}
+
+	if raw[1] == 'f' {
+		return false, nil
+	}
+
+	return false, fmt.Errorf("Failed to parse bool (%q)", raw)
+}
+
+func deserialiseArray(raw string) ([]interface{}, error) {
+	toParse := string(raw[1:])
+	valuesToParse := 0
+	rawValues := ""
+	values := []interface{}{}
+
+	for i := 0; i < len(toParse); i++ {
+		if isCRLF(toParse, i) {
+			if rawValues == "" || rawValues == "0" {
+				return values, nil
+			}
+			toParse = toParse[i+2:]
+			break
+		}
+		rawValues += string(toParse[i])
+	}
+
+	valuesToParse, err := strconv.Atoi(rawValues)
+	if err != nil {
+		return values, fmt.Errorf("Failed to parse values (%q) err: %w", rawValues, err)
+	}
+
+	//NOTE:
+	// figuere out how to preserve a pointer between different deserialise functions
+	for i := 0; i < len(toParse); i++ {
+	}
+	
+	return values, nil
+}
+
 func Deserialise(raw string) (interface{}, error)  {
 	switch raw[0] {
 	case '+':
@@ -76,8 +137,14 @@ func Deserialise(raw string) (interface{}, error)  {
 		return deserialiseErrorString(raw)
 	case '$':
 		return deserialiseBulkString(raw)
-	case '*':
+	case ':':
+		return deserialiseInteger(raw)
+	case '#':
+		return deserialiseBool(raw)
+	case '_': // null
 		return nil, nil
+	case '*':
+		return deserialiseArray(raw)
 	}
 
 	return "", fmt.Errorf("unknown type: %#v", raw[0])
